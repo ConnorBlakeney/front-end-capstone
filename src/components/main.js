376ba601aa32b4268cf037bb89923362c616317c
Connor Blakeney <connorblakeney@yahoo.com>
2020-09-14 10:53:50 -0500

    added fight scroll folder

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from '../serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>

0c03d11f3b7aadcdc8297ca145f24333da5b7d09
Connor Blakeney <connorblakeney@yahoo.com>
2020-09-17 20:27:47 -0500

    renders to the dom

--- src/components/messages/Message.js
+++ src/components/messages/Message.js
@@ -3,24 +3,24 @@ import { MessageContext } from "./MessageProvider";
 import { UserContext } from "../users/UserProvider";
 import "./Message.css"
 
-export default ({ props  }) => {
+export default ({ props, message  }) => {
     const currentUserId = parseInt(localStorage.getItem("current_user"))
-    const {getUsers, users } = useContext(UserContext)
-    const {getMessages, messages } = useContext(MessageContext)
-    const [user, setUser] = useState({})
-    const [message, setMessage] = useState({})
+    // const {getUsers, users } = useContext(UserContext)
+    // const {getMessages, messages } = useContext(MessageContext)
+    // const [user, setUser] = useState({})
+    // const [message, setMessage] = useState({})
 
-    useEffect(() => {
-        getUsers().then(getMessages)
-    }, [])
+    // useEffect(() => {
+    //     getUsers().then(getMessages)
+    // }, [])
 
-    useEffect(() => {
-        setUser(users)
-    }, [users])
+    // useEffect(() => {
+    //     setUser(users)
+    // }, [users])
 
-    useEffect(() => {
-        setMessage(messages)
-    }, [messages])
+    // useEffect(() => {
+    //     setMessage(messages)
+    // }, [messages])
 
     return (   
         <section key={message.id} className="message">
--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -29,6 +29,7 @@ export const MessageList = ({ history, props }) => {
     
     useEffect(() => {
         setMessage(messages)
+        console.log(message)
     }, [messages])
     
 

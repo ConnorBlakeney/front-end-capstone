d44f7c92a53170fe9ca45b590c34b07ee94fc5b7
Connor Blakeney <connorblakeney@yahoo.com>
2020-09-19 21:28:34 -0500

    added fight identifier to dropdown

--- src/components/scores/Scores.js
+++ src/components/scores/Scores.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react"
-import "./Scores.css"
+// import React, { useState } from "react"
+// import "./Scores.css"
 
-export default ({ fight, user, score }) => (
-    <section key={score.id} className="score">
-        <h3 className="score__fight">
-                { fight.R_fighter } vs { fight.B_fighter }          
-        </h3>
-    </section>
-)
+// export default ({ fight, user, score }) => (
+//     <section key={score.id} className="score">
+//         <h3 className="score__fight">
+//                 { fight.R_fighter } vs { fight.B_fighter }          
+//         </h3>
+//     </section>
+// )

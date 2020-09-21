56e5a8e48fcfc22e8bbaf6f3afc6e88655d238d3
Connor Blakeney <connorblakeney@yahoo.com>
2020-09-21 09:25:35 -0500

    sends scores data to dom

--- src/components/scores/ScoresForm.js
+++ src/components/scores/ScoresForm.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState, useRef} from "react"
 import { UserContext } from "../users/UserProvider";
 // import { FriendContext } from "./FriendsProvider";
-import { ScoresDropDownFights, ScoresDropDownFriends } from "./ScoresDropDown";
+import { FightContext } from "../fights/FightProvider";
 import "./Scores.css"
 import { ScoresList } from "./ScoresList";
 
@@ -9,6 +9,7 @@ import { ScoresList } from "./ScoresList";
 export default ({ friend, image, title  }) => {
     const currentUserId = parseInt(localStorage.getItem("current_user"))
     const {getUsers, users } = useContext(UserContext)
+    const {getFights, fights } = useContext(FightContext)
     const [user, setUser] = useState({})
 
 
@@ -19,27 +20,12 @@ export default ({ friend, image, title  }) => {
     return (  
     
         
-        <section className="scores">
+        <section className="scores form">
             
             {/* <ScoresDropDownFights />  */}
-            <ScoresList />
 
-            <section className="card__form">    
-                <form className="round__form red">
-                    <input className="round__one red" placeholder="Round 1 Red" type="number"></input>
-                    <input className="round__two red" placeholder="Round 2 Red" type="number"></input>
-                    <input className="round__three red" placeholder="Round 3 Red" type="number"></input>
-                    <input className="round__four red" placeholder="Round 4 Red" type="number"></input>
-                    <input className="round__five red" placeholder="Round 5 Red" type="number"></input>
-                </form>
-                <form className="round__form red">
-                    <input className="round__one blue" placeholder="Round 1 Blue" type="number"></input>
-                    <input className="round__two blue" placeholder="Round 2 Blue" type="number"></input>
-                    <input className="round__three blue" placeholder="Round 3 Blue" type="number"></input>
-                    <input className="round__four blue" placeholder="Round 4 Blue" type="number"></input>
-                    <input className="round__five blue" placeholder="Round 5 Blue" type="number"></input>
-                </form>
-            </section>
+            <ScoresList />
+            
         </section>
     )
 
--- src/components/scores/ScoresList.js
+++ src/components/scores/ScoresList.js
@@ -9,7 +9,7 @@ import "./Scores.css"
 
 export const ScoresList = ({ history, props }) => {
     const { getUsers, users } = useContext(UserContext)
-    const { getScore, scores } = useContext(ScoreContext)
+    const { getScore, scores, addScore } = useContext(ScoreContext)
     const { getFights, fights } = useContext(FightContext)
     const { getFriends, friends } = useContext(FriendContext)
 
@@ -28,6 +28,23 @@ export const ScoresList = ({ history, props }) => {
         getScore().then(getUsers).then(getFights).then(getFriends)
     }, [])
     
+    const handleSubmit = (e) => {
+        addScore ({
+                    userId: currentUserId,
+                    roundOneRed: parseInt(document.getElementById('roundOneRed').value),
+                    roundOneBlue: parseInt(document.getElementById('roundOneBlue').value),
+                    roundTwoRed: parseInt(document.getElementById('roundTwoRed').value),
+                    roundTwoBlue: parseInt(document.getElementById('roundTwoBlue').value),
+                    roundThreeRed: parseInt(document.getElementById('roundThreeRed').value),
+                    roundThreeBlue: parseInt(document.getElementById('roundThreeBlue').value),
+                    roundFourRed: parseInt(document.getElementById('roundFourRed').value),
+                    roundFourBlue: parseInt(document.getElementById('roundFourBlue').value),
+                    roundFiveRed: parseInt(document.getElementById('roundFiveRed').value),
+                    roundFiveBlue: parseInt(document.getElementById('roundFiveBlue').value),
+                    scoreFightId: fight.id,
+                })
+        e.preventDefault()
+    }
     // useEffect(() => {
     //     setUser(users)
     // }, [users])
@@ -46,7 +63,7 @@ export const ScoresList = ({ history, props }) => {
 
     return (
 
-    <div class="round__form total" key={score.id}>
+    <div className="scores" key={score.id}>
 
         <div className="filteredFriends">
             <label>
@@ -66,15 +83,27 @@ export const ScoresList = ({ history, props }) => {
                     }
                 </select>
             </label>
-        </div>
 
-        {/* <form className="round__form blue">
-            <input className="round__one blue" type="number"></input>
-            <input className="round__two blue" type="number"></input>
-            <input className="round__three blue" type="number"></input>
-            <input className="round__four blue" type="number"></input>
-            <input className="round__five blue" type="number"></input>
-        </form> */}
+        <section className="card__form">    
+                <form className="round__form red">
+                    <input id="roundOneRed" className="round__one red" placeholder="Round 1 Red" type="number"></input>
+                    <input id="roundTwoRed" className="round__two red" placeholder="Round 2 Red" type="number"></input>
+                    <input id="roundThreeRed" className="round__three red" placeholder="Round 3 Red" type="number"></input>
+                    <input id="roundFourRed" className="round__four red" placeholder="Round 4 Red" type="number"></input>
+                    <input id="roundFiveRed" className="round__five red" placeholder="Round 5 Red" type="number"></input>
+                </form>
+                <form className="round__form red">
+                    <input id="roundOneBlue" className="round__one blue" placeholder="Round 1 Blue" type="number"></input>
+                    <input id="roundTwoBlue" className="round__two blue" placeholder="Round 2 Blue" type="number"></input>
+                    <input id="roundThreeBlue" className="round__three blue" placeholder="Round 3 Blue" type="number"></input>
+                    <input id="roundFourBlue"className="round__four blue" placeholder="Round 4 Blue" type="number"></input>
+                    <input id="roundFiveBlue" className="round__five blue" placeholder="Round 5 Blue" type="number"></input>
+                    <button id={fight.id} onClick={
+                        handleSubmit
+                    }>Save Scores</button>
+                </form>
+            </section>
+        </div>
        
     </div>
     )

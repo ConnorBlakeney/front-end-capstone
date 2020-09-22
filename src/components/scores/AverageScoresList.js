70a59ff92e38dcc82702045b72ec41b7cccadf89
Connor Blakeney <connorblakeney@yahoo.com>
2020-09-22 16:12:18 -0500

    shows average for every fight

--- src/components/scores/AverageScoresList.js
+++ src/components/scores/AverageScoresList.js
@@ -45,27 +45,64 @@ export const AverageScoresList = ({ history, props }) => {
 
     const handleSubmit = (e) => {
         const fightSelect = parseInt(fightId.current.value)
+
+        // map over users, . to scores, save scores as array of scores
         const scoreFind = scores.find(s => s.scoreFightId === fightSelect) || {}
         const scoreFilter = scores.filter(s => s.scoreFightId === fightSelect) || {}
+        const averageScoreOneBlue = scoreFilter.map(s => s.roundOneBlue).reduce((a, b) => a + b, 0) / scoreFilter.length
+        const averageScoreTwoBlue = scoreFilter.map(s => s.roundTwoBlue).reduce((a, b) => a + b, 0) / scoreFilter.length
+        const averageScoreThreeBlue = scoreFilter.map(s => s.roundThreeBlue).reduce((a, b) => a + b, 0) / scoreFilter.length
+        const averageScoreFourBlue = scoreFilter.map(s => s.roundFourBlue).reduce((a, b) => a + b, 0) / scoreFilter.length
+        const averageScoreFiveBlue = scoreFilter.map(s => s.roundFiveBlue).reduce((a, b) => a + b, 0) / scoreFilter.length
+        const averageScoreOneRed = scoreFilter.map(s => s.roundOneRed).reduce((a, b) => a + b, 0) / scoreFilter.length
+        const averageScoreTwoRed = scoreFilter.map(s => s.roundTwoRed).reduce((a, b) => a + b, 0) / scoreFilter.length
+        const averageScoreThreeRed = scoreFilter.map(s => s.roundThreeRed).reduce((a, b) => a + b, 0) / scoreFilter.length
+        const averageScoreFourRed = scoreFilter.map(s => s.roundFourRed).reduce((a, b) => a + b, 0) / scoreFilter.length
+        const averageScoreFiveRed = scoreFilter.map(s => s.roundFiveRed).reduce((a, b) => a + b, 0) / scoreFilter.length
         // const filteredScoresForCard = filteredScoresForCurrentUser.filter(s => s.scoreFightId === fightSelect)
 
         // if (currentUserId === scoreFind.userId && fightSelect === scoreFind.scoreFightId) {
         //    return <OldScoresList />
-        console.log(scoreFilter)
+        console.log(scoreFilter, averageScoreOneBlue, averageScoreTwoBlue, averageScoreThreeBlue, averageScoreFourBlue, averageScoreFiveBlue)
         // const scoreEl = document.querySelector("#filtered__average__scores")
         // scoreEl.innerHTML = ""
         // if (fightSelect === scoreFind.scoreFightId) {
         //                           console.log(scores, scoreFind.roundOneBlue, fightId, fightSelect)                                                          
 
-                               setFilteredScores( scoreFilter.map(score => {
-                            return ( 
+
+
+                                    setFilteredScores( fightSelect ===  scoreFind.scoreFightId  ? <section key={score.id} className="score">
+                                    <div className="score__card">
+                                        <div className="score__fight red">
+                                                <p className="round__one red">Round 1: { averageScoreOneRed }</p>           
+                                                <p className="round__two red">Round 2: { averageScoreTwoRed }</p>         
+                                                <p className="round__three red">Round 3: { averageScoreThreeRed } </p>         
+                                                <p className="round__four red">Round 4: { averageScoreFourRed } </p>       
+                                                <p className="round__five red">Round 5: { averageScoreFiveRed } </p>        
+                                        </div>
+                                        <div className="score__fight blue">
+                                                <p className="round__one blue"> - { averageScoreOneBlue }</p>         
+                                                <p className="round__one blue"> - { averageScoreTwoBlue }</p>        
+                                                <p className="round__one blue"> - { averageScoreThreeBlue }</p>         
+                                                <p className="round__one blue"> - { averageScoreFourBlue }</p>         
+                                                <p className="round__one blue"> - { averageScoreFiveBlue }</p>         
+                                        </div>
+                                    </div>
+                                </section> : "" )
+                    //            setFilteredScores( scoreFilter.map(score => {
+                    //         return ( 
+                    //                console.log(score)
                             
-                                 <ScoresUser className="score__option" key={score.id} id={score.id} score={score} {...props} /> 
-                            )
-                    }) )
+                    //             //  <ScoresUser className="score__option" key={score.id} id={score.id} score={score} {...props} /> 
+                                                                                            
+                    //         )
+                    // }) 
+                                 
+                            
+                    
                                                                                             
                           
-        // }
+        
 
         // for(key in scoreFind) {
         //     if (key.scoreFightId === fightSelect) {

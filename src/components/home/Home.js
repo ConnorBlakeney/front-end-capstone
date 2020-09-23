b6f6274c69c0bac26538cff650de5630437f7a2e
Connor Blakeney <connorblakeney@yahoo.com>
2020-09-23 16:07:19 -0500

    cleaned up home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,12 +2,20 @@ import React from "react"
 import "./Home.css"
 import { Link } from "react-router-dom"
 
+// jsx renders indivdual fights with links
 export default ({ fight }) => (
+
     <section key={fight.id} className="fight">
+
         <h3 className="fight__name">
-            <Link to={`/fights/${fight.id}`}   >
+
+            <Link to={`/fights/${fight.id}`}>
+
                 { fight.R_fighter } vs { fight.B_fighter }
+
             </Link>
+
         </h3>
+
     </section>
 )
--- src/components/home/HomeForm.js
+++ src/components/home/HomeForm.js
@@ -1,32 +1,34 @@
-import React, { useContext, useState, useEffect } from "react"
-import { FightContext } from "../fights/FightProvider";
+import React from "react"
 import { UpcomingList, RecentList } from "./HomeList";
 import "./Home.css"
 
-export const HomeForm = (props) => {
-    // Use the required context providers for data
-    const { fights, getFights } = useContext(FightContext)
+export const HomeForm = () => {
 
-    // Component state
-    const [fight, setFight] = useState({})
-
-    
-    useEffect(() => {
-        getFights()
-    }, [])
-
+    // jsx that renders home page
     return (
-        <form className="homeForm">
-            <h2 className="homeForm__title">Home Page</h2>
+
+        <form className="home__form">
+
+            <h2 className="home__form__title">Home Page</h2>
+
             <div className="fight__cards">
+
                 <fieldset className="form__upcoming card">
+
                     <h3>Upcoming Main Card Fights</h3>
+
                     <UpcomingList/>
+
                 </fieldset>
+
                 <fieldset className="form__previous card">
+
                     <h3>Previous Main Card Fights</h3>
+
                     <RecentList/>
+
                 </fieldset>
+
             </div>
             
         </form>
